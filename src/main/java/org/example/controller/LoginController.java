7138e49755b14a0bbee018891550b52d2e4f3a3f
david <david.mitrea@gmail.com>
2025-03-31 21:08:01 +0300

    Found a bug:)

--- src/main/java/org/example/controller/LoginController.java
+++ src/main/java/org/example/controller/LoginController.java
@@ -47,7 +47,6 @@ public class LoginController {
             clearFields();
             usernameErrorLabel.setText("Username not found");
         }
-
     }
 
     private void openMainPannel(Cashier cashier) {
